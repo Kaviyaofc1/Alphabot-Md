9cf6aa567b4d5c7128a4483bfea4ff3918d05544
Kaviya_ofc <111883653+Kaviyaofc1@users.noreply.github.com>
2023-04-28 13:24:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -46,17 +46,17 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "Kaviyaofc-Mdོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄ KaviyaOfcོ ×፝֟͜×" //nama kalian
 global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://s.id/zeeoneofc" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
+global.timezone = 'Asia/Colombo' //  timezone wib
+global.premium = ['94775418384'] //premium user
+global.owner = ["94775418384", "94775418384"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "94775418384" // nomor wa kalian
 global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
